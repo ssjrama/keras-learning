0f353292641a539e9e1554a054937be0656e6f6c
Wahyu Ramadhan <ssjrama@gmail.com>
2022-08-02 09:34:08 +0700

    update

--- flask/app.py
+++ flask/app.py
@@ -38,5 +38,10 @@ def predict_rice_ctrl():
     return jsonify({'prediction': predict_rice(features)})
 
 
+@app.route("/")
+def home_view():
+    return "<h1>Hello World</h1>"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
