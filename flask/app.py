01c4bd6864c4c06aa8f2ed3acbc3d26063ed2ae5
Wahyu Ramadhan <ssjrama@gmail.com>
2022-08-04 19:23:16 +0700

    update

--- flask/app.py
+++ flask/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 # Preload our rice model
 print('Loading rice model...')
 rice_graph = tf.compat.v1.get_default_graph()
+fertilizer_graph = tf.compat.v1.get_default_graph()
 
 
 def predict_rice(features):
@@ -26,17 +27,32 @@ def predict_rice(features):
         input_dict = {name: tf.convert_to_tensor(
             [value]) for name, value in sample.items()}
         prediction = rice_model.predict(input_dict, steps=50)
-
-        #prediction = rice_model.predict(np.array(features))
         print('prediction: ', prediction[0, 0])
         return np.float64(prediction[0, 0])
+
+
+def predict_fertilizer(features):
+    with rice_graph.as_default():
+        model = load_model('./model/model-f.h5')
+
+        a2D = np.array(
+            [[features[0], features[1], features[2], 3, 4, features[3], features[4], features[5]]])
+
+        y_pred_new = np.argmax(model.predict(a2D), axis=-1)
+        print('prediction: ', y_pred_new[0])
+        return np.int(y_pred_new[0])
 
 
 @app.route('/rice/predict', methods=['POST'])
 def predict_rice_ctrl():
     features = request.get_json()['features']
-    print(features[0])
     return jsonify({'prediction': predict_rice(features)})
+
+
+@app.route('/fertilizer/predict', methods=['POST'])
+def predict_fertilizer_ctrl():
+    features = request.get_json()['features']
+    return jsonify({'prediction': predict_fertilizer(features)})
 
 
 @app.route("/")
